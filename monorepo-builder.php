6b6541149c6e76e7e4f594c12adab2c39fc1f74c
ryuring <egashira@catchup.co.jp>
2024-06-16 14:49:49 +0000

    open 0.1-dev

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -15,18 +15,20 @@ return static function (MBConfig $mbConfig): void {
     $version = $_SERVER['argv'][2];
     
     if(preg_match('/^[0-9]+\.[0-9]+\.[0-9]+$/', $version)) {
+    	$mbConfig->disableDefaultWorkers();
     	// 正式リリース
+		$mbConfig->workers([
+			UpdateReplaceReleaseWorker::class,
+			SetCurrentMutualDependenciesReleaseWorker::class,
+			AddTagToChangelogReleaseWorker::class,
+			SetNextMutualDependenciesReleaseWorker::class,
+			UpdateBranchAliasReleaseWorker::class,
+			PushNextDevReleaseWorker::class
+		]);
     } elseif(preg_match('/^[0-9]+\.[0-9]+\.[0-9]+-(alpha|beta|rc|dev)/', $version)) {
     	// alpha / beta / rc
     	$mbConfig->defaultBranch('dev');
     }
     
-    $mbConfig->workers([
-//		UpdateReplaceReleaseWorker::class,
-//		SetCurrentMutualDependenciesReleaseWorker::class,
-//		AddTagToChangelogReleaseWorker::class,
-//		SetNextMutualDependenciesReleaseWorker::class,
-//		UpdateBranchAliasReleaseWorker::class,
-//		PushNextDevReleaseWorker::class
-	]);
+
 };
