3c711c68891d2ad90f59036cb86bd8979f2eaa64
ryuring <egashira@catchup.co.jp>
2024-06-16 14:00:01 +0000

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,8 +3,22 @@
 declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
     $mbConfig->packageDirectories([__DIR__ . '/plugins']);
     $mbConfig->defaultBranch('dev');
+    $mbConfig->workers([
+//		UpdateReplaceReleaseWorker::class,
+//		SetCurrentMutualDependenciesReleaseWorker::class,
+//		AddTagToChangelogReleaseWorker::class,
+//		SetNextMutualDependenciesReleaseWorker::class,
+//		UpdateBranchAliasReleaseWorker::class,
+//		PushNextDevReleaseWorker::class
+	]);
 };
